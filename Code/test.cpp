d12b0f3bea9c177eb8b7d1f1df77844070c4c67d
UnrealLink <37017725+UnrealLink@users.noreply.github.com>
2019-05-09 13:30:52 +0200

    test update
    Added option choice for testing

--- Code/test.cpp
+++ Code/test.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdio.h>
+#include <stdlib.h>
 #include "Dataset.hpp"
 #include "PSSMatrix.hpp"
 
 int main(int argc, const char * argv[]){
 
-    // DATASET TEST
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <0 (for Dataset) | 1 (for PSSMatrix)>" << std::endl;
+        return 1;
+    }
 
-	Dataset dataset("../Data/EUKSIG_13.red");
+    int option = atoi(argv[1]);
+
+    // DATASET TEST
     
-	dataset.show(false);
+    if (option == 0) {
+
+        Dataset dataset("../Data/EUKSIG_13.red");
+        
+        dataset.show(true);
+
+    }
 
     // PSSMATRIX TEST
 
-    /* 
-    MASKATLLLAFTLLFATCIARHQQRQQQQNQCQLQNIEALEPIEVIQAEA
-    SSSSSSSSSSSSSSSSSSSSCMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-    */
+    if (option == 1) {
 
-    int p = 10;
-    int q = 2;
-    PSSMatrix pssmatrix(dataset, p, q);
+        Dataset dataset("../Data/EUKSIG_13.red");
+        int p = 10;
+        int q = 2;
+        PSSMatrix pssmatrix(dataset, p, q);
 
-    Data data = dataset.get(1);
-    string sequence;
-    for (int i = p; i<data.sequence.size()-q; i++) {
-        sequence = data.sequence.substr(i-p, p+q);
-        cout<<"Sequence "<< sequence;
-        if (i == data.cleavage) cout<<" contains cleavage site";
-        cout<<". Score :"<<endl<<pssmatrix.score(sequence);
-        if (pssmatrix.classify(sequence)) cout<<" -> should contain cleavage site";
-        cout<<endl;
+        Data data = dataset.get(1);
+        string sequence;
+        for (int i = p; i<data.sequence.size()-q; i++) {
+            sequence = data.sequence.substr(i-p, p+q);
+            cout<<"Sequence "<< sequence;
+            if (i == data.cleavage) cout<<" contains cleavage site";
+            cout<<". Score :"<<endl<<pssmatrix.score(sequence);
+            if (pssmatrix.classify(sequence)) cout<<" -> should contain cleavage site";
+            cout<<endl;
+        }
     }
 	
 	return 0;
